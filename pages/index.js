72cbdbfd8f513b952d73aa4a3b6ed7af127b42a7
Oscar <oscarantonio.soto1998@gmail.com>
2023-03-04 20:33:08 -0300

    Fix: router and links in nextjes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <>
       <h1>Hola mundo desde Next.js</h1>
+      <Link href="/nosotros">Nosotros</Link>
     </>
   );
 }
---
+++ pages/nosotros.js
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const Nosotros = () => {
+  return (
+    <div>
+      <Link href="/">Ir al inicio</Link>
+    </div>
+  )
+}
+
+export default Nosotros;
