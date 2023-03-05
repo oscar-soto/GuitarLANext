18a284703dfd9c9eedd31fcf11069c70b0bd1283
Oscar <oscarantonio.soto1998@gmail.com>
2023-03-05 20:43:55 -0300

    Fix: add style to us pages

--- components/header.js
+++ components/header.js
@@ -2,7 +2,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 // import logo from '../public/img/logo.svg'
-import styles from '../styles/header.module.css'
+import styles from '../styles/header.module.css';
 
 const Header = () => {
   const router = useRouter();
@@ -12,29 +12,46 @@ const Header = () => {
       <div className={`container ${styles.bar}`}>
         {/* <Image src={logo.src}  width={300} height={40} alt='Logo del GuitarLA'/> */}
         <Link href={'/'}>
-          <Image src="/img/logo.svg"  width={300} height={40} alt='Logo del GuitarLA'/>
+          <Image
+            src="/img/logo.svg"
+            width={300}
+            height={40}
+            alt="Logo del GuitarLA"
+          />
         </Link>
 
         <nav className={styles.navigation}>
-          <Link  className={ router.pathname === '/' ? styles.active : ''} href="/">
+          <Link
+            className={router.pathname === '/' ? styles.active : ''}
+            href="/"
+          >
             Inicio
           </Link>
 
-          <Link className={ router.pathname === '/nosotros' ? styles.active : ''} href="/nosotros">
+          <Link
+            className={router.pathname === '/nosotros' ? styles.active : ''}
+            href="/nosotros"
+          >
             Nosotros
           </Link>
 
-          <Link className={ router.pathname === '/blog' ? styles.active : ''} href="/blog">
+          <Link
+            className={router.pathname === '/blog' ? styles.active : ''}
+            href="/blog"
+          >
             Blog
           </Link>
 
-          <Link className={ router.pathname === '/tienda' ? styles.active : ''} href="/tienda">
+          <Link
+            className={router.pathname === '/tienda' ? styles.active : ''}
+            href="/tienda"
+          >
             Tienda
           </Link>
         </nav>
       </div>
     </header>
-  )
-}
+  );
+};
 
 export default Header;
--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,4 +1,6 @@
-import Layout from "@/components/layout";
+import Layout from '@/components/layout';
+import styles from '../styles/nosotros.module.css';
+import Image from 'next/image';
 
 const Nosotros = () => {
   return (
@@ -6,9 +8,39 @@ const Nosotros = () => {
       title={'Nosotros'}
       description={'Sobre nosotros, GuitarLA, tienda de música'}
     >
-      <h1>Nosotros</h1>
+      <main className="container">
+        <h2 className="heading">Nosotros</h2>
+
+        <div className={styles.content}>
+          <Image
+            src="/img/nosotros.jpg"
+            alt="Imagen sobre nosotros"
+            width={1000}
+            height={800}
+          />
+
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo amet
+              rem, sed perferendis nostrum consectetur fugit voluptatem repellat
+              esse, molestiae facere reprehenderit dolore odit pariatur.
+              Perferendis error veniam iure officia quidem, nobis autem nisi
+              quas beatae. Id praesentium molestiae numquam atque nemo, magnam
+              eveniet totam est, ipsam nesciunt nisi nulla.
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo amet
+              rem, sed perferendis nostrum consectetur fugit voluptatem repellat
+              esse, molestiae facere reprehenderit dolore odit pariatur.
+              Perferendis error veniam iure officia quidem, nobis autem nisi
+              quas beatae. Id praesentium molestiae numquam atque nemo, magnam
+              eveniet totam est, ipsam nesciunt nisi nulla.
+            </p>
+          </div>
+        </div>
+      </main>
     </Layout>
-  )
-}
+  );
+};
 
 export default Nosotros;
