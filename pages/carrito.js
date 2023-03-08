ae02b473447294d75f1854b0dd30b6949b22b384
Oscar <oscarantonio.soto1998@gmail.com>
2023-03-08 14:14:23 -0300

    Fix: add style to cart and select for modificate the qty

--- pages/carrito.js
+++ pages/carrito.js
@@ -2,8 +2,8 @@ import Layout from '@/components/layout';
 import styles from '@/styles/carrito.module.css';
 import Image from 'next/image';
 
-const Carrito = ({ cart }) => {
-  console.log(cart)
+const Carrito = ({ cart, updateQty }) => {
+  
   return (
     <Layout title="Carrito de Compras">
       <main className="container">
@@ -28,9 +28,30 @@ const Carrito = ({ cart }) => {
 
                     <div>
                       <p className={styles.name}>{product.name}</p>
+
+                      <div className={styles.qty}>
+                        <p>Cantidad:</p>
+
+                        <select
+                          className={styles.select}
+                          onChange={(e) => updateQty({
+                            id: product.id,
+                            qty: e.target.value
+                          })}
+                          value={product.qty}
+                        >
+                          <option value="1">1</option>
+                          <option value="2">2</option>
+                          <option value="3">3</option>
+                          <option value="4">4</option>
+                          <option value="5">5</option>
+                        </select>
+                      </div>
+
                       <p className={styles.price}>
                         $<span>{product.price}</span>
                       </p>
+
                       <p className={styles.subtotal}>
                         Subtotal: $<span>{product.qty * product.price}</span>
                       </p>
