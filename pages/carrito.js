5c34a03871842c42c5a61156a6b2bd331e389b66
Oscar <oscarantonio.soto1998@gmail.com>
2023-03-08 13:55:29 -0300

    Fix: show products in the cart0

--- pages/carrito.js
+++ pages/carrito.js
@@ -1,15 +1,42 @@
 import Layout from '@/components/layout';
 import styles from '@/styles/carrito.module.css';
+import Image from 'next/image';
 
-const Carrito = () => {
+const Carrito = ({ cart }) => {
+  console.log(cart)
   return (
-    <Layout title='Carrito de Compras'>
+    <Layout title="Carrito de Compras">
       <main className="container">
         <h1 className="heading">Carrito</h1>
 
         <div className={styles.content}>
           <div className={styles.cart}>
             <h2>Artículos</h2>
+
+            {cart.length === 0
+              ? 'Carrito Vacio'
+              : cart.map((product) => (
+                  <div key={product.id} className={styles.product}>
+                    <figure>
+                      <Image
+                        width={250}
+                        height={480}
+                        src={product.image}
+                        alt={product.name}
+                      />
+                    </figure>
+
+                    <div>
+                      <p className={styles.name}>{product.name}</p>
+                      <p className={styles.price}>
+                        $<span>{product.price}</span>
+                      </p>
+                      <p className={styles.subtotal}>
+                        Subtotal: $<span>{product.qty * product.price}</span>
+                      </p>
+                    </div>
+                  </div>
+                ))}
           </div>
 
           <aside className={styles.summary}>
